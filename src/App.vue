6fa9d083ca9fb96be0c5124ebc1152576882ec6f
Duverney <duverney.ortiz@servinformacion.com>
2023-07-11 17:05:25 -0500

    Show only Clock

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    {{ hours }} - {{ date }}
-    <InputDateTime v-model="date" :max="2023" :min="2022"></InputDateTime>
     <VueClock v-model="hours"></VueClock>
   </div>
 </template>
 
 <script>
-import InputDateTime from '@/InputDateTime.vue'
+// import InputDateTime from '@/InputDateTime.vue'
 import VueClock from '@/components/Clock.vue'
 
 export default {
   name: 'App',
   components: {
-    InputDateTime,
+    // InputDateTime,
     VueClock
   },
   data: () => ({
