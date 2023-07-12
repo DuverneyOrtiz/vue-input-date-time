fd4afc819bb5920844f54eabfba725e450a09f03
DuverneyOrtiz <duverneyortiz@hotmail.com>
2023-07-11 19:20:11 -0500

    Update dist build project

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,3 @@
 export { default as VueInputDateTime } from './InputDateTime/index.js';
 export { default as VueClock } from  './Clock/index.js';
+export { default as VueClockMilitar } from  './Militar/index.js';
