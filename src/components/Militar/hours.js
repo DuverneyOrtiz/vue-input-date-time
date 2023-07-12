4e984fcd2a74f60e318fb0ff79c88741dc020bc4
DuverneyOrtiz <duverneyortiz@hotmail.com>
2023-07-11 19:22:00 -0500

    Component to render clock militar

---
+++ src/components/Militar/hours.js
@@ -0,0 +1,206 @@
+export default [
+    {
+        attr: {
+            className: "number-0-12",
+        },
+        hours: [
+            {
+                hour: '0',
+                value: '0',
+                conditionClass: "00",
+            },
+            {
+                hour: '12',
+                value: '12',
+                conditionClass: "12",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-1-13",
+        },
+        hours: [
+            {
+                hour: '1',
+                value: '1',
+                conditionClass: "01",
+            },
+            {
+                hour: '13',
+                value: '13',
+                conditionClass: "13",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-2-14",
+        },
+        hours: [
+            {
+                hour: '2',
+                value: '2',
+                conditionClass: "02",
+            },
+            {
+                hour: '14',
+                value: '14',
+                conditionClass: "14",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-3-15",
+        },
+        hours: [
+            {
+                hour: '3',
+                value: '3',
+                conditionClass: "03",
+            },
+            {
+                hour: '15',
+                value: '15',
+                conditionClass: "15",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-4-16",
+        },
+        hours: [
+            {
+                hour: '4',
+                value: '4',
+                conditionClass: "04",
+            },
+            {
+                hour: '16',
+                value: '16',
+                conditionClass: "16",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-5-17",
+        },
+        hours: [
+            {
+                hour: '5',
+                value: '5',
+                conditionClass: "05",
+            },
+            {
+                hour: '17',
+                value: '17',
+                conditionClass: "17",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-6-18",
+        },
+        hours: [
+            {
+                hour: '6',
+                value: '6',
+                conditionClass: "06",
+            },
+            {
+                hour: '18',
+                value: '18',
+                conditionClass: "18",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-7-19",
+        },
+        hours: [
+            {
+                hour: '7',
+                value: '7',
+                conditionClass: "07",
+            },
+            {
+                hour: '19',
+                value: '19',
+                conditionClass: "19",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-8-20",
+        },
+        hours: [
+            {
+                hour: '8',
+                value: '8',
+                conditionClass: "08",
+            },
+            {
+                hour: '20',
+                value: '20',
+                conditionClass: "20",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-9-21",
+        },
+        hours: [
+            {
+                hour: '9',
+                value: '9',
+                conditionClass: "09",
+            },
+            {
+                hour: '21',
+                value: '21',
+                conditionClass: "21",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-10-22",
+        },
+        hours: [
+            {
+                hour: '10',
+                value: '10',
+                conditionClass: "10",
+            },
+            {
+                hour: '22',
+                value: '22',
+                conditionClass: "22",
+            },
+        ],
+    },
+    {
+        attr: {
+            className: "number-11-23",
+        },
+        hours: [
+            {
+                hour: '11',
+                value: '11',
+                conditionClass: "11",
+            },
+            {
+                hour: '23',
+                value: '23',
+                conditionClass: "23",
+            },
+        ],
+    },
+]
